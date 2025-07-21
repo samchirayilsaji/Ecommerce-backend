43ce68d7126696814f789f1ef340b2abf5e52ef6
Sam Chirayil <samchirayil@Sams-MacBook-Pro.local>
2025-07-21 22:41:56 +0530

    Fix: added root route and verified routers for deployment

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 from fastapi import FastAPI
-from routes import orders
-from routes import product
 from routes.product import router as product_router
 from routes.orders import router as order_router
 
-
 app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"message": "Backend running"}
 
 app.include_router(product_router)
 app.include_router(order_router)
-@app.get("/")
-def root():
-    return {"message": "Ecommerce Backend API is running 🚀"}
