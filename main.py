75b46bf5c999d04b3d4bae856013ec698fa4a50b
Sam Chirayil <samchirayil@Sams-MacBook-Pro.local>
2025-07-21 22:36:32 +0530

    Add root route for homepage

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from routes import orders
+from routes import product
 from routes.product import router as product_router
 from routes.orders import router as order_router
 
@@ -7,3 +9,6 @@ app = FastAPI()
 
 app.include_router(product_router)
 app.include_router(order_router)
+@app.get("/")
+def root():
+    return {"message": "Ecommerce Backend API is running 🚀"}
